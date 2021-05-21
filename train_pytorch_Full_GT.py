dd81bd1a1dbb2b3678e0cd2e72af806be190fd02
Dai Quoc Nguyen <dai.nguyen@monash.edu>
2021-05-21 19:40:15 +1000

    Release Fully-Connected Graph Transformer

---
+++ train_pytorch_Full_GT.py
@@ -0,0 +1,155 @@
+#! /usr/bin/env python
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+torch.manual_seed(123)
+
+import numpy as np
+np.random.seed(123)
+import time
+
+from pytorch_FC_GT import *
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+from scipy.sparse import coo_matrix
+from util import *
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(123)
+
+# Parameters
+# ==================================================
+
+parser = ArgumentParser("U2GNN", formatter_class=ArgumentDefaultsHelpFormatter, conflict_handler='resolve')
+
+parser.add_argument("--run_folder", default="../", help="")
+parser.add_argument("--dataset", default="PTC", help="Name of the dataset.")
+parser.add_argument("--learning_rate", default=0.0005, type=float, help="Learning rate")
+parser.add_argument("--num_epochs", default=50, type=int, help="Number of training epochs")
+parser.add_argument("--model_name", default='PTC', help="")
+parser.add_argument("--dropout", default=0.5, type=float, help="")
+parser.add_argument("--num_hidden_layers", default=1, type=int, help="")
+parser.add_argument("--num_timesteps", default=1, type=int, help="Timestep T ~ Number of self-attention layers within each U2GNN layer")
+parser.add_argument("--ff_hidden_size", default=256, type=int, help="The hidden size for the feedforward layer")
+parser.add_argument('--fold_idx', type=int, default=1, help='The fold index. 0-9.')
+args = parser.parse_args()
+
+print(args)
+
+# Load data
+print("Loading data...")
+
+use_degree_as_tag = False
+if args.dataset == 'COLLAB' or args.dataset == 'IMDBBINARY' or args.dataset == 'IMDBMULTI':
+    use_degree_as_tag = True
+graphs, num_classes = load_data(args.dataset, use_degree_as_tag)
+# graph_labels = np.array([graph.label for graph in graphs])
+# train_idx, test_idx = separate_data_idx(graphs, args.fold_idx)
+train_graphs, test_graphs = separate_data(graphs, args.fold_idx)
+feature_dim_size = graphs[0].node_features.shape[1]
+print(feature_dim_size)
+if "REDDIT" in args.dataset:
+    feature_dim_size = 4
+
+def get_Adj_matrix(graph):
+    Adj_block_idx = torch.LongTensor(graph.edge_mat)
+    Adj_block_elem = torch.ones(Adj_block_idx.shape[1])
+
+    num_node = len(graph.g)
+    self_loop_edge = torch.LongTensor([range(num_node), range(num_node)])
+    elem = torch.ones(num_node)
+    Adj_block_idx = torch.cat([Adj_block_idx, self_loop_edge], 1)
+    Adj_block_elem = torch.cat([Adj_block_elem, elem], 0)
+
+    Adj_block = torch.sparse.FloatTensor(Adj_block_idx, Adj_block_elem, torch.Size([num_node, num_node]))
+
+    return Adj_block.to(device) # implement the Laplacian re-normalized adjacency matrix like in GCN ???
+
+def get_data(graph):
+    node_features = graph.node_features
+    if "REDDIT" in args.dataset:
+        node_features = np.tile(node_features, feature_dim_size) #[1,1,1,1]
+        node_features = node_features * 0.01
+    node_features = torch.from_numpy(node_features).to(device)
+    Adj_block = get_Adj_matrix(graph)
+    graph_label = np.array([graph.label])
+    return Adj_block, node_features, torch.from_numpy(graph_label).to(device)
+
+# Adj_block, node_features, graph_label = get_data(train_graphs[1])
+# print(Adj_block)
+# print(node_features)
+# print(graph_label)
+
+print("Loading data... finished!")
+
+model = FullyConnectedGT(feature_dim_size=feature_dim_size, ff_hidden_size=args.ff_hidden_size,
+                        num_classes=num_classes, dropout=args.dropout,
+                        num_self_att_layers=args.num_timesteps,
+                        num_U2GNN_layers=args.num_hidden_layers).to(device)
+
+def cross_entropy(pred, soft_targets): # use nn.CrossEntropyLoss if not using soft labels in Line 159
+    logsoftmax = nn.LogSoftmax(dim=1)
+    return torch.mean(torch.sum(- soft_targets * logsoftmax(pred), 1))
+# criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
+
+def train():
+    model.train() # Turn on the train mode
+    total_loss = 0.
+    idxs = np.arange(len(train_graphs))
+    np.random.shuffle(idxs)
+    for idx in idxs:
+        Adj_block, node_features, graph_label = get_data(train_graphs[idx]) # one graph per step, i.e., bs=1
+        graph_label = label_smoothing(graph_label, num_classes)
+        optimizer.zero_grad()
+        prediction_score = model.forward(Adj_block, node_features) # forward or simple_forward?
+        # loss = criterion(prediction_scores, graph_labels)
+        loss = cross_entropy(torch.unsqueeze(prediction_score, 0), graph_label)
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5) # prevent the exploding gradient problem
+        optimizer.step()
+        total_loss += loss.item()
+
+    return total_loss
+
+def evaluate():
+    model.eval() # Turn on the evaluation mode
+    total_loss = 0.
+    with torch.no_grad():
+        # evaluating
+        prediction_output = []
+        for i in range(0, len(test_graphs)):
+            Adj_block, node_features, graph_label = get_data(test_graphs[i])
+            prediction_score = model.forward(Adj_block, node_features).detach()
+            prediction_output.append(torch.unsqueeze(prediction_score, 0))
+    prediction_output = torch.cat(prediction_output, 0)
+    predictions = prediction_output.max(1, keepdim=True)[1]
+    labels = torch.LongTensor([graph.label for graph in test_graphs]).to(device)
+    correct = predictions.eq(labels.view_as(predictions)).sum().cpu().item()
+    acc_test = correct / float(len(test_graphs))
+    return acc_test
+
+"""main process"""
+import os
+out_dir = os.path.abspath(os.path.join(args.run_folder, "../runs_pytorch_FC_GraphTransformer", args.model_name))
+print("Writing to {}\n".format(out_dir))
+# Checkpoint directory
+checkpoint_dir = os.path.abspath(os.path.join(out_dir, "checkpoints"))
+checkpoint_prefix = os.path.join(checkpoint_dir, "model")
+if not os.path.exists(checkpoint_dir):
+    os.makedirs(checkpoint_dir)
+write_acc = open(checkpoint_prefix + '_acc.txt', 'w')
+
+cost_loss = []
+for epoch in range(1, args.num_epochs + 1):
+    epoch_start_time = time.time()
+    train_loss = train()
+    cost_loss.append(train_loss)
+    acc_test = evaluate()
+    print('| epoch {:3d} | time: {:5.2f}s | loss {:5.2f} | test acc {:5.2f} | '.format(
+                epoch, (time.time() - epoch_start_time), train_loss, acc_test*100))
+
+    write_acc.write('epoch ' + str(epoch) + ' fold ' + str(args.fold_idx) + ' acc ' + str(acc_test*100) + '%\n')
+
+write_acc.close()
---
+++ util.py
@@ -0,0 +1,200 @@
+import networkx as nx
+import numpy as np
+import random
+import scipy.sparse as sp
+from sklearn.model_selection import StratifiedKFold
+import torch
+torch.manual_seed(123)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(123)
+np.random.seed(123)
+
+########
+def normalize_sparse(mx):
+    """Row-normalize sparse matrix"""
+    rowsum = np.array(mx.sum(1))
+    r_inv = np.power(rowsum, -1).flatten()
+    r_inv[np.isinf(r_inv)] = 0.
+    r_mat_inv = sp.diags(r_inv)
+    mx = r_mat_inv.dot(mx)
+    return mx
+
+def sparse_mx_to_torch_sparse_tensor(sparse_mx):
+    """Convert a scipy sparse matrix to a torch sparse tensor."""
+    sparse_mx = sparse_mx.tocoo().astype(np.float32)
+    indices = torch.from_numpy(
+        np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64))
+    values = torch.from_numpy(sparse_mx.data)
+    shape = torch.Size(sparse_mx.shape)
+    return torch.sparse.FloatTensor(indices, values, shape)
+
+
+"""Adapted from https://github.com/weihua916/powerful-gnns/blob/master/util.py"""
+
+class S2VGraph(object):
+    def __init__(self, g, label, node_tags=None, node_features=None):
+        '''
+            g: a networkx graph
+            label: an integer graph label
+            node_tags: a list of integer node tags
+            node_features: a torch float tensor, one-hot representation of the tag that is used as input to neural nets
+            edge_mat: a torch long tensor, contain edge list, will be used to create torch sparse tensor
+            neighbors: list of neighbors (without self-loop)
+        '''
+        self.label = label
+        self.g = g
+        self.node_tags = node_tags
+        self.neighbors = []
+        self.node_features = 0
+        self.edge_mat = 0
+        self.max_neighbor = 0
+
+
+def load_data(dataset, degree_as_tag):
+    '''
+        dataset: name of dataset
+        test_proportion: ratio of test train split
+        seed: random seed for random splitting of dataset
+    '''
+
+    print('loading data')
+    g_list = []
+    label_dict = {}
+    feat_dict = {}
+
+    with open('../dataset/%s/%s.txt' % (dataset, dataset), 'r') as f:
+        n_g = int(f.readline().strip())
+        for i in range(n_g):
+            row = f.readline().strip().split()
+            n, l = [int(w) for w in row]
+            if not l in label_dict:
+                mapped = len(label_dict)
+                label_dict[l] = mapped
+            g = nx.Graph()
+            node_tags = []
+            node_features = []
+            n_edges = 0
+            for j in range(n):
+                g.add_node(j)
+                row = f.readline().strip().split()
+                tmp = int(row[1]) + 2
+                if tmp == len(row):
+                    # no node attributes
+                    row = [int(w) for w in row]
+                    attr = None
+                else:
+                    row, attr = [int(w) for w in row[:tmp]], np.array([float(w) for w in row[tmp:]])
+                if not row[0] in feat_dict:
+                    mapped = len(feat_dict)
+                    feat_dict[row[0]] = mapped
+                node_tags.append(feat_dict[row[0]])
+
+                if tmp > len(row):
+                    node_features.append(attr)
+
+                n_edges += row[1]
+                for k in range(2, len(row)):
+                    g.add_edge(j, row[k])
+
+            if node_features != []:
+                node_features = np.stack(node_features)
+                node_feature_flag = True
+            else:
+                node_features = None
+                node_feature_flag = False
+
+            assert len(g) == n
+
+            g_list.append(S2VGraph(g, l, node_tags))
+
+    #add labels and edge_mat       
+    for g in g_list:
+        g.neighbors = [[] for i in range(len(g.g))]
+        for i, j in g.g.edges():
+            g.neighbors[i].append(j)
+            g.neighbors[j].append(i)
+        degree_list = []
+        for i in range(len(g.g)):
+            g.neighbors[i] = g.neighbors[i]
+            degree_list.append(len(g.neighbors[i]))
+        g.max_neighbor = max(degree_list)
+
+        g.label = label_dict[g.label]
+
+        edges = [list(pair) for pair in g.g.edges()]
+        edges.extend([[i, j] for j, i in edges])
+
+        deg_list = list(dict(g.g.degree(range(len(g.g)))).values())
+
+        g.edge_mat = np.transpose(np.array(edges, dtype=np.int32), (1,0))
+
+    if degree_as_tag:
+        for g in g_list:
+            g.node_tags = list(dict(g.g.degree).values())
+
+    #Extracting unique tag labels   
+    tagset = set([])
+    for g in g_list:
+        tagset = tagset.union(set(g.node_tags))
+
+    tagset = list(tagset)
+    tag2index = {tagset[i]:i for i in range(len(tagset))}
+
+    for g in g_list:
+        g.node_features = np.zeros((len(g.node_tags), len(tagset)), dtype=np.float32)
+        g.node_features[range(len(g.node_tags)), [tag2index[tag] for tag in g.node_tags]] = 1
+
+
+    print('# classes: %d' % len(label_dict))
+    print('# maximum node tag: %d' % len(tagset))
+
+    print("# data: %d" % len(g_list))
+
+    return g_list, len(label_dict)
+
+def separate_data(graph_list, fold_idx, seed=0):
+    assert 0 <= fold_idx and fold_idx < 10, "fold_idx must be from 0 to 9."
+    skf = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
+
+    labels = [graph.label for graph in graph_list]
+    idx_list = []
+    for idx in skf.split(np.zeros(len(labels)), labels):
+        idx_list.append(idx)
+    train_idx, test_idx = idx_list[fold_idx]
+
+    train_graph_list = [graph_list[i] for i in train_idx]
+    test_graph_list = [graph_list[i] for i in test_idx]
+
+    return train_graph_list, test_graph_list
+
+"""Get indexes of train and test sets"""
+def separate_data_idx(graph_list, fold_idx, seed=0):
+    assert 0 <= fold_idx and fold_idx < 10, "fold_idx must be from 0 to 9."
+    skf = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
+
+    labels = [graph.label for graph in graph_list]
+    idx_list = []
+    for idx in skf.split(np.zeros(len(labels)), labels):
+        idx_list.append(idx)
+    train_idx, test_idx = idx_list[fold_idx]
+
+    return train_idx, test_idx
+
+"""Convert sparse matrix to tuple representation."""
+def sparse_to_tuple(sparse_mx):
+    def to_tuple(mx):
+        if not sp.isspmatrix_coo(mx):
+            mx = mx.tocoo()
+        coords = np.vstack((mx.row, mx.col)).transpose()
+        values = mx.data
+        shape = mx.shape
+        return coords, values, shape
+
+    if isinstance(sparse_mx, list):
+        for i in range(len(sparse_mx)):
+            sparse_mx[i] = to_tuple(sparse_mx[i])
+    else:
+        sparse_mx = to_tuple(sparse_mx)
+
+    return sparse_mx
+
